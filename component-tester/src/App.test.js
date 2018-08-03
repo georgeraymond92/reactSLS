7fa1ea8140075ed3d218c7405ae19d8794d5f7fb
george <geomarray@gmail.com>
2018-08-03 16:57:18 -0700

    added the component tester react app begining the construction of components

--- component-tester/src/App.test.js
+++ component-tester/src/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import header from './header';
+import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<header />, div);
+  ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
---
+++ component-tester/src/main.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Main = () => (
+    <header class="masthead">
+      <div class="container">
+        <div class="intro-text">
+          <div class="intro-lead-in">Welcome To Our Website!</div>
+          <div class="intro-heading text-uppercase">Senior Living Service</div>
+          <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#nownow">Find a Place Now!</a>
+        </div>
+      </div>
+    </header>
+);
+
+export default Main;
