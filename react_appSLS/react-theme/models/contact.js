278135219ce6f32a557fa41f0915daf3acea0bfc
george <geomarray@gmail.com>
2018-08-07 20:30:57 -0700

    working on the routes and the axios post

--- react_appSLS/react-theme/models/contact.js
+++ react_appSLS/react-theme/models/contact.js
@@ -1,13 +1,30 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const articleSchema = new Schema({
+const contactSchema = new Schema({
   _id: { type: Schema.Types.ObjectId, required: true },
-  title: { type: String, required: true },
-  url: { type: String, required: true },
-  date: { type: Date, default: Date.now }
+  name: { type: String, required: true },
+  serv: { type: String, required: true },
+  phone: { type: Number,  min: 10, max: 10, required: true },
+  email: { type: String, required: true },
+  location: { type: Number, min: 5, max: 5, required: true },
 }, { _id: false });
 
-const Article = mongoose.model("Article", articleSchema);
+const Contact = mongoose.model("Contact", contactSchema);
 
-module.exports = Article;
+module.exports = Contact;
+
+
+
+// title: { type: String, required: true },
+// url: { type: String, required: true },
+// date: { type: Date, default: Date.now }
+
+// const articleSchema = new Schema({
+//   _id: { type: Schema.Types.ObjectId, required: true },
+//   name: { type: String, required: true },
+//   serv: { type: String, required: true },
+//   phone: { type: Number,  min: 10, max: 10, required: true },
+//   email: { type: String, required: true },
+//   location: { type: Number, min: 5, max: 5, required: true },
+// }, { _id: false });
--- react_appSLS/react-theme/src/components/form.js
+++ react_appSLS/react-theme/src/components/form.js
@@ -1,58 +1,83 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios'
 
-const Form = () => (
-    <div>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <div className="container" id="nownow">
-            <div className="wrapper animated bounceInLeft">
-                <div className="company-info" >
-                    <h3 >Senior Living Service</h3>
-                    <ul>
-                        <li><i className="fa fa-road"></i> Contact Now!</li>
-                        <li><i className="fa fa-phone"></i> (858) 633-3590</li>
-                        <li><i className="fa fa-envelope"></i> seniorlivingervices@gmail.com</li>
-                    </ul>
+class Form extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: "",
+            email: "",
+            phone: "",
+            location: "",
+            serv: ""
+        }
+        this.handleFormSubmit = this.handleFormSubmit.bind(this);   
+    }
+    handleFormSubmit (e) {
+        e.preventDefault();
+        console.log('handleing the submit');
+        axios.post('api/contacts', this.state)
+        .then(function(data){
+            console.log(data)
+        }).catch(function(err){
+            console.log(err);
+        });
+    }
+    render(){
+        return (
+            <div>
+                <br />
+                <br />
+                <br />
+                <br />
+                <div className="container" id="nownow">
+                    <div className="wrapper animated bounceInLeft">
+                        <div className="company-info" >
+                            <h3 >Senior Living Service</h3>
+                            <ul>
+                                <li><i className="fa fa-road"></i> Contact Now!</li>
+                                <li><i className="fa fa-phone"></i> (858) 633-3590</li>
+                                <li><i className="fa fa-envelope"></i> seniorlivingervices@gmail.com</li>
+                            </ul>
+                        </div>
+                        <div id="results" className="contact">
+                            <h3>Find a Place, Services, & Location</h3>
+                            <form>
+                                <p>
+                                    <label>Name</label>
+                                    <input id="name" onChange={this.state.name} placeholder="Enter your name" type="text" name="name"/>
+                                </p>
+                                <p>
+                                    <label>Service</label>
+                                    <select onChange={this.state.serv} id="services" list="services5">
+                                        <option value="Independant Living">Independant Living</option>
+                                        <option value="Assisted Living">Assisted Living</option>
+                                        <option value="Memory Care">Memory Care</option>
+                                    </select>
+                                </p>
+                                <p>
+                                    <label>Email Address</label>
+                                    <input id="email" onChange={this.state.email} placeholder="Email" type="text" name="email"/>
+                                </p>
+                                <p>
+                                    <label>Phone Number</label>
+                                    <input id="phone" onChange={this.state.phone} placeholder="Phone Number" type="text" name="phone"/>
+                                </p>
+                                <p>
+                                    <label>Location:</label>
+                                    <input id="location" onChange={this.state.location} placeholder="Enter your name" type="text" name="location"/>
+                                </p>
+                                <p className="full">
+                                    <button id="submitBtn" onClick={this.handleFormSubmit}>Submit</button>
+                                </p>
+                            </form>
+                        </div>
+                    </div>
                 </div>
-                <div id="results" className="contact">
-                    <h3>Find a Place, Services, & Location</h3>
-                    <form>
-                        <p>
-                            <label>Name</label>
-                            <input id="name" type="text" name="name"/>
-                        </p>
-                        <p>
-                            <label>Service</label>
-                            <select id="services" list="services5">
-                                <option value="Independant Living">Independant Living</option>
-                                <option value="Assisted Living">Assisted Living</option>
-                                <option value="Memory Care">Memory Care</option>
-                            </select>
-                        </p>
-                        <p>
-                            <label>Email Address</label>
-                            <input id="email"  type="email" name="email"/>
-                        </p>
-                        <p>
-                            <label>Phone Number</label>
-                            <input id="phone" type="text" name="phone"/>
-                        </p>
-                        <p>
-                            <label>Location:</label>
-                            <input type="text" id="location" name="location"/>
-                        </p>
-                        <p className="full">
-                            <button id="submitBtn" >Submit</button>
-                        </p>
-                    </form>
-                </div>
+                <div className="absolute"></div>
             </div>
-        </div>
-        <div className="absolute"></div>
-    </div>
-
-);
+        )
+    }    
+}
 
 export default Form;
