2bd933228ad805838813c7d2b9c43422df68f850
kayhon <kayhon@gmail.com>
2018-08-09 21:01:43 -0700

    last_P

---
+++ react_appSLS/react-theme/dist/login/js/custom2.js
@@ -0,0 +1,24 @@
+// var MongoClient = require('mongodb').MongoClient;
+// var url = 'mongodb://localhost/SLS_db';
+
+// MongoClient.connect(url, function(err, db) {
+
+//     var cursor = db.collection('contacts').find();
+
+//     cursor.each(function(err, doc) {
+
+//         console.log(doc);
+
+//     });
+// }); 
+
+
+
+
+
+// $(document).ready(function(){
+
+
+
+  
+// });
